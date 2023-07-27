96d522ccf174c5ba2bb21b3e7aa50af860477991
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-27 09:34:55 +0300

    state and useEffect for fetching data

--- src/components/GitHubRepositoryExplorer.js
+++ src/components/GitHubRepositoryExplorer.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from 'react';
 import './GitHubRepositoryExplorer.css'; // Import the CSS file
 
 const GitHubRepositoryExplorer = () => {
+  const [repositories, setRepositories] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    fetchMostStarredRepositories();
+  }, []);
+
 
 
 
